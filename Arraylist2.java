8406ba5bb0233f19d746e8955e9513b333ae738c
Ruchi <ruchi27tiwari@gmail.com>
2024-05-28 14:46:51 +0530

    remove item from arraylist2

---
+++ Arraylist2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Arraylist2 {
+    public static void main(String[] args) 
+    {
+    List<Float> a=new  ArrayList <Float>() ;  
+
+    a.add(2.4f);
+    a.add(2.2f);
+    a.add(3.4f);
+    a.add(4.2f);
+  //  for (float n: a)
+  
+  //{
+    System.out.println(a);
+
+    //}
+    a.remove(1);
+    System.out.println(a);
+}
+}
+    
+//}
