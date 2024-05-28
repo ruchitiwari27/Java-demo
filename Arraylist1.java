d9cb72b9a2456b4eb94536bebd486ee0818a7718
Ruchi <ruchi27tiwari@gmail.com>
2024-05-28 14:15:09 +0530

    ArrayList first program

---
+++ Arraylist1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arraylist1 {
+    public static void main(String[] args) {
+    Collection <Integer>num =new ArrayList<Integer>(); 
+        num.add(3);
+        num.add(4);
+        num.add(5);
+        num.add(6);
+
+        System.out.println(num);
+    }
+    
+}
