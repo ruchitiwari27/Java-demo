984e3f3647e7c1bac3a01de364f64190a6145d3c
Ruchi <ruchi27tiwari@gmail.com>
2024-05-29 13:34:53 +0530

    using array sort() to sort array

---
+++ Sortarray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Sortarray {
+    public static void main(String[] args) {
+        int [] arr={1,2,35,4,7,5,};
+       // arr.sort(arr)
+       Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+           // Arrays.sort(arr);
+            System.out.println(arr[i]);
+        }
+    }
+    
+ }
+// public class Sortarray  {
+//     public static void main(String[] args) {
+//       int[] myNum = {50, 10, 25, 1, 17, 99, 33};
+//       //Arrays.sort(myNum);
+//       sort(myNum);
+//       for (int i : myNum) {
+//         System.out.println(i);
+//       }
+//     }
+//   }
